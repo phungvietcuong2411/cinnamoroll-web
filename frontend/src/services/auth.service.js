180003d86bfe4596c1c4d4658d33e1b620a338d5
phungvietcuong2411 <chitoge241103@gmail.com>
2025-12-28 22:20:02 +0700

    28 - 12 - 2025 2nd  commit

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "./Axios/axios"
 
 export const login = async (data) => {
-  const res = await axios.post("/auth/login", data)
+  const res = await axios.post("api/auth/login", data)
 
   if (res.data.token) {
     localStorage.setItem("token", res.data.token)
--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,21 +1,21 @@
 import axios from "./Axios/axios"
 
 export const getAllUsers = () => {
-  return axios.get("/users")
+  return axios.get("api/users")
 }
 
 export const getUserById = (id) => {
-  return axios.get(`/users/${id}`)
+  return axios.get(`api/users/${id}`)
 }
 
 export const createUser = (data) => {
-  return axios.post("/users", data)
+  return axios.post("api/users", data)
 }
 
 export const updateUser = (id, data) => {
-  return axios.put(`/users/${id}`, data)
+  return axios.put(`api/users/${id}`, data)
 }
 
 export const deleteUser = (id) => {
-  return axios.delete(`/users/${id}`)
+  return axios.delete(`api/users/${id}`)
 }
